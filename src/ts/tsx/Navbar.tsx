e72b24c6754d9b83a4585715661b75b4e2a11775
wont.stream <drns.pw@icloud.com>
2024-10-26 19:48:40 -0400

    uhhhhhh??

--- src/ts/tsx/Navbar.tsx
+++
@@ -1,9 +0,0 @@
-export const Navbar = () => {
-	return (
-		<header>
-			<nav>
-				<h5 class="max center-align">ipv4.army</h5>
-			</nav>
-		</header>
-	);
-};
--- src/ts/tsx/Tidal.tsx
+++ src/ts/tsx/Tidal.tsx
@@ -7,11 +7,6 @@ export const Tidal = () => {
 	on("discord", (activity: Activity) => {
 		if (link.current) {
 			if (activity.listening_to_tidal) {
-<<<<<<< HEAD:src/ts/tsx/Tidal.tsx
-				img.current.style.background = `center / contain no-repeat url(https://wsrv.nl/?output=webp&q=1&url=${activity.tidal.album_art_url})`;
-				blur.current.classList.add("small-blur");
-=======
->>>>>>> f495ae3 (update):src/tsx/Tidal.tsx
 				link.current.href = `https://tidal.com/browse/track/${activity.tidal.trackId}/u`;
 				link.current.innerHTML = `Listening to<br>${activity.tidal.song.replace(
 					/\s?[\(\[].*?[\)\]]/g,
