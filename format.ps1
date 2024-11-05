5bf47d5259fe15c9db0e2ddc7e2ad13747538a03
wont-stream <143244075+wont-stream@users.noreply.github.com>
2024-11-05 15:27:03 -0500

    whoops

--- format.ps1
+++ format.ps1
@@ -1,3 +1,3 @@
 ./node_modules/.bin/biome.exe lint --write
 ./node_modules/.bin/biome.exe format --write
-git commit -am "Format & Lint Code - format.ps1 w/ biome - $(date)"
+git commit -m "Format & Lint Code - format.ps1 w/ biome - $(date)"
