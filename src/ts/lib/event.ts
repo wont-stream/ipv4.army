8ee7770ca096c986a1388422948a66b6f996c20d
wont-stream <143244075+wont-stream@users.noreply.github.com>
2024-11-05 15:16:21 -0500

    remove debug logs

--- src/ts/lib/event.ts
+++ src/ts/lib/event.ts
@@ -3,13 +3,11 @@ type Event = { name: string; callback: Function };
 const events: Event[] = [];
 
 export const on = async (name: Event["name"], callback: Event["callback"]) => {
-	console.log("Event registered:", name);
 	return events.push({ name, callback });
 };
 
 export const emit = async (name: string, data: Activity | Tidal | string) => {
 	for (const event of events.filter((event) => event.name === name)) {
-		console.log("Event emitted:", name, data);
 		return await event.callback(data);
 	}
 };
