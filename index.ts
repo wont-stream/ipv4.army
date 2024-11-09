6cf42bd9a0b4731918301cb89360ba939d88620d
wont-stream <143244075+wont-stream@users.noreply.github.com>
2024-11-09 17:42:02 -0500

    Simplify response creation for index.html and streamline file handling

--- index.ts
+++ index.ts
@@ -7,14 +7,10 @@ serve({
 		const { pathname } = new URL(req.url);
 
 		if (pathname === "/") {
-			return new Response(await file("./dist/index.html").bytes(), {
-				headers: {
-					"Content-Type": "text/html; charset=utf-8",
-				},
-			});
+			return new Response(await file("./dist/index.html"));
 		}
 
-		const possibleFile = Bun.file(`./dist${pathname}`)
+		const possibleFile = file(`./dist${pathname}`)
 
 		if (await possibleFile.exists()) {
 			return new Response(possibleFile);
