9fc83cc5fcb02051971c2a264fc039cac965c18f
seth <git@ipv4.army>
2025-05-19 01:21:40 -0400

    add easteregg

--- src/front/index.tsx
+++ src/front/index.tsx
@@ -3,12 +3,25 @@ import "tsx-dom";
 import "./utilities/clicker";
 
 import App from "./App";
-
 import colors from "./utilities/colors.module.css";
+import { snacker } from "./utilities/snackbar";
 
 document.documentElement.className = colors.offline || "";
 
 document.body.appendChild(<App />);
+
+let clicks = 0;
+let resetCount = "";
+document.onclick = () => {
+    clicks++;
+
+    if (clicks > 5) {
+        snacker({
+            message: `Please stop.${resetCount}`,
+        });
+        resetCount += ".";
+    }
+};
 
 // You're garbage, let me collect you.
 fetch("/api/gc");
