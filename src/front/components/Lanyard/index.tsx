088ed2a3993bbb65b4ffebfbfb73a61bb0d2130f
seth <git@ipv4.army>
2025-06-18 20:26:58 -0400

    fix: update image URL size parameter in getImageUrl function

--- src/front/components/Lanyard/index.tsx
+++ src/front/components/Lanyard/index.tsx
@@ -19,7 +19,7 @@ const getImageUrl = (activity: LanyardActivity, size: "large" | "small") => {
 	const width = size === "large" ? 120 : 40;
 	if (!activity.assets || !activity.assets.large_image) {
 		if (size === "large")
-			return `https://dcdn.dstn.to/app-icons/${activity.application_id}?size=128`;
+			return `https://dcdn.dstn.to/app-icons/${activity.application_id}?size=${width}`;
 	}
 
 	if (!activity.assets) return null;
