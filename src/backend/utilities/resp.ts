2b4b673c9ef5b2c8177c6ddd0a0311b1a42e94ed
seth <git@ipv4.army>
2025-07-14 02:47:22 -0400

    run biome

--- src/backend/utilities/resp.ts
+++ src/backend/utilities/resp.ts
@@ -1,41 +1,40 @@
 const respOptions = {
-    headers: {
-        "Content-Type": "application/json",
-        "Cache-Control": "no-cache",
-        "Content-Encoding": "gzip",
-    },
+	headers: {
+		"Content-Type": "application/json",
+		"Cache-Control": "no-cache",
+		"Content-Encoding": "gzip",
+	},
 };
 
-export const ok = () => new Response(
-    Bun.gzipSync(JSON.stringify({ data: "ok" })),
-    respOptions,
-);
+export const ok = () =>
+	new Response(Bun.gzipSync(JSON.stringify({ data: "ok" })), respOptions);
 
 export const json = (data: { [key: string]: string }) =>
-    new Response(Bun.gzipSync(JSON.stringify(data)), respOptions);
+	new Response(Bun.gzipSync(JSON.stringify(data)), respOptions);
 
 export const file = async (file: Bun.BunFile) => {
-    const isHTML = file.type === "text/html";
+	const isHTML = file.type === "text/html";
 
-    return new Response(Bun.gzipSync(await file.arrayBuffer()), {
-        headers: {
-            "Content-Type": file.type,
-            ...(isHTML
-                ? { }
-                : { "Cache-Control": "public, max-age=31536000" }),
-            "Content-Encoding": "gzip",
-        },
-    });
-}
-export const text = async (text: string, contentType: string, status = 200, noCache = false) => {
-    return new Response(Bun.gzipSync(text), {
-        status,
-        headers: {
-            "Content-Type": contentType,
-            ...(noCache
-                ? { }
-                : { "Cache-Control": "public, max-age=31536000" }),
-            "Content-Encoding": "gzip",
-        },
-    });
-}
+	return new Response(Bun.gzipSync(await file.arrayBuffer()), {
+		headers: {
+			"Content-Type": file.type,
+			...(isHTML ? {} : { "Cache-Control": "public, max-age=31536000" }),
+			"Content-Encoding": "gzip",
+		},
+	});
+};
+export const text = async (
+	text: string,
+	contentType: string,
+	status = 200,
+	noCache = false,
+) => {
+	return new Response(Bun.gzipSync(text), {
+		status,
+		headers: {
+			"Content-Type": contentType,
+			...(noCache ? {} : { "Cache-Control": "public, max-age=31536000" }),
+			"Content-Encoding": "gzip",
+		},
+	});
+};
