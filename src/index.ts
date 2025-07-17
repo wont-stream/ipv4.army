df30014a0222213d60049b3ed7c3a07405c113bc
seth <git@ipv4.army>
2025-07-17 02:42:49 -0400

    log ip for testing

--- src/index.ts
+++ src/index.ts
@@ -69,7 +69,8 @@ const server = Bun.serve({
 	hostname: process.env.HOSTNAME || "localhost",
 
 	routes: {
-		"/": async (req: Bun.BunRequest<"/">, _server: Bun.Server) => {
+		"/": async (req: Bun.BunRequest<"/">, server: Bun.Server) => {
+			console.log(server.requestIP(req)?.address);
 			updatePartials();
 
 			return await response.text(
