26fed6ace215252736f3def4611327bd73a32873
seth <git@ipv4.army>
2025-07-25 01:34:30 -0400

    Add more manifest icons

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -88,9 +88,23 @@ export default defineConfig({
 			"link",
 			{
 				rel: "icon",
-				href: "/favicon.svg",
+				type: "image/png",
+				href: "/favicon-96x96.png",
+				sizes: "96x96",
 			},
 		],
+		["link", { rel: "icon", type: "image/svg+xml", href: "/favicon.svg" }],
+		["link", { rel: "shortcut icon", href: "/favicon.ico" }],
+		[
+			"link",
+			{
+				rel: "apple-touch-icon",
+				sizes: "180x180",
+				href: "/apple-touch-icon.png",
+			},
+		],
+		["meta", { name: "apple-mobile-web-app-title", content: "S€TH" }],
+		["link", { rel: "manifest", href: "/site.webmanifest" }],
 	],
 	metaChunk: true,
 
