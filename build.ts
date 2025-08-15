97a72fbc601f42f0b126db42abe10fdfd6ba5e21
seth <git@ipv4.army>
2025-08-15 00:36:31 -0400

    refactor build process: remove unnecessary build and streamline import

--- build.ts
+++ build.ts
@@ -1,17 +1,7 @@
 await Bun.$`bun docs:build`;
 
-await Bun.build({
-	entrypoints: ["index.ts"],
-	outdir: "dist",
-	target: "bun",
-	format: "cjs",
-	env: "inline",
-	minify: true,
-	bytecode: true,
-});
-
-console.clear();
+console.log("------------------------");
 
 Bun.gc(true);
 
-await import("./dist/index.js");
+await import("./index.ts");
