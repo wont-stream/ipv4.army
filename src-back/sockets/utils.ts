8ff9b7bcfa503776c068e40be339c0c2d3882d32
Seth <git@ipv4.army>
2025-10-18 20:13:02 -0400

    Increase websocket reconnection delay and backoff

--- src-back/sockets/utils.ts
+++ src-back/sockets/utils.ts
@@ -1,8 +1,8 @@
 import type { Options } from "reconnecting-websocket";
 
 export const websocketOptions: Options = {
-	minReconnectionDelay: 1000,
-	reconnectionDelayGrowFactor: 0,
-	connectionTimeout: 5000,
-	maxRetries: Number.MAX_SAFE_INTEGER,
+  minReconnectionDelay: 5000,
+  reconnectionDelayGrowFactor: 1.5,
+  connectionTimeout: 5000,
+  maxRetries: Number.MAX_SAFE_INTEGER,
 };
