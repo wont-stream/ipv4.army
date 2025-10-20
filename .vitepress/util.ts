6fb142c8b100860c68667b25cb8da2b7e1577ab4
Seth <git@ipv4.army>
2025-10-20 04:21:24 -0400

    comment stream badge out

--- .vitepress/util.ts
+++ .vitepress/util.ts
@@ -1,99 +1,99 @@
 import { readdirSync } from "node:fs";
 
 const months = [
-	"",
-	"January",
-	"February",
-	"March",
-	"April",
-	"May",
-	"June",
-	"July",
-	"August",
-	"September",
-	"October",
-	"November",
-	"December",
+  "",
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
 ];
 
 type Item = {
-	text: string;
-	items: { text: string; link?: string; items?: Item[] }[];
+  text: string;
+  items: { text: string; link?: string; items?: Item[] }[];
 };
 
 const getBlogSidebar = () => {
-	const filesAndDirs = readdirSync("./src/blog", {
-		withFileTypes: true,
-		recursive: true,
-	});
+  const filesAndDirs = readdirSync("./src/blog", {
+    withFileTypes: true,
+    recursive: true,
+  });
 
-	const files = filesAndDirs
-		.filter((file) => file.isFile() && file.name.endsWith(".md"))
-		.map(
-			(file) =>
-				`${file.parentPath.replaceAll("\\", "/").replace("src/blog/", "")}/${file.name.replace(".md", "")}`,
-		);
+  const files = filesAndDirs
+    .filter((file) => file.isFile() && file.name.endsWith(".md"))
+    .map(
+      (file) =>
+        `${file.parentPath.replaceAll("\\", "/").replace("src/blog/", "")}/${file.name.replace(".md", "")}`,
+    );
 
-	const items: Item[] = [];
+  const items: Item[] = [];
 
-	for (const post of files) {
-		const [year, month, day, title] = post.split("/") as [
-			string,
-			string,
-			string,
-			string,
-		];
+  for (const post of files) {
+    const [year, month, day, title] = post.split("/") as [
+      string,
+      string,
+      string,
+      string,
+    ];
 
-		let yearItem = items.find((item) => item.text === year);
-		if (!yearItem) {
-			yearItem = { text: year, items: [] };
-			items.push(yearItem);
-		}
+    let yearItem = items.find((item) => item.text === year);
+    if (!yearItem) {
+      yearItem = { text: year, items: [] };
+      items.push(yearItem);
+    }
 
-		let monthItem = yearItem.items.find(
-			(item) => item.text === months[parseInt(month)],
-		);
-		if (!monthItem) {
-			monthItem = { text: months[parseInt(month)] as string, items: [] };
-			yearItem.items.push(monthItem);
-		}
+    let monthItem = yearItem.items.find(
+      (item) => item.text === months[parseInt(month)],
+    );
+    if (!monthItem) {
+      monthItem = { text: months[parseInt(month)] as string, items: [] };
+      yearItem.items.push(monthItem);
+    }
 
-		if (monthItem.items) {
-			let dayItem = monthItem.items.find((item) => item.text === day);
-			if (!dayItem) {
-				dayItem = { text: day.slice(1), items: [] };
-				monthItem.items.push(dayItem);
-			}
+    if (monthItem.items) {
+      let dayItem = monthItem.items.find((item) => item.text === day);
+      if (!dayItem) {
+        dayItem = { text: day.slice(1), items: [] };
+        monthItem.items.push(dayItem);
+      }
 
-			const titleParts = title.split("-");
-			dayItem.items.push({
-				text: titleParts[1]?.replace("_", " ") as string,
-				link: `/blog/${post}`,
-			});
-		}
-	}
+      const titleParts = title.split("-");
+      dayItem.items.push({
+        text: titleParts[1]?.replace("_", " ") as string,
+        link: `/blog/${post}`,
+      });
+    }
+  }
 
-	return items;
+  return items;
 };
 export const blogItems = getBlogSidebar();
 
 const badges = [
-	{ name: "Heartrate", path: "heartrate" },
-	{ name: "Listening To", path: "listening" },
-	{ name: "Discord Status", path: "status" },
-	{ name: "Livestream", path: "stream" },
-	{ name: "Coding Time Weekly", path: "waka_week" },
-	{ name: "Coding Time Monthly", path: "waka_month" },
+  { name: "Heartrate", path: "heartrate" },
+  { name: "Listening To", path: "listening" },
+  { name: "Discord Status", path: "status" },
+  //{ name: "Livestream", path: "stream" },
+  { name: "Coding Time Weekly", path: "waka_week" },
+  { name: "Coding Time Monthly", path: "waka_month" },
 ];
 
 const getBadges = () => {
-	let html = "";
+  let html = "";
 
-	for (const badge of badges) {
-		html += `<img src="/badge/${badge.path}" alt="${badge.name}" />`;
-	}
+  for (const badge of badges) {
+    html += `<img src="/badge/${badge.path}" alt="${badge.name}" />`;
+  }
 
-	return html;
+  return html;
 };
 
 export const badgesHTML = getBadges();
