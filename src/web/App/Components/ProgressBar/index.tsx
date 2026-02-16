2e636f6aea7cdde8b1ed0168efdef719428331b9
Seth <git@ipv4.army>
2026-02-15 23:17:06 -0500

    run lint

--- src/web/App/Components/ProgressBar/index.tsx
+++ src/web/App/Components/ProgressBar/index.tsx
@@ -3,74 +3,74 @@ import "./index.css";
 import { useEffect, useState } from "preact/hooks";
 
 export const ProgressBar = (props: { activity: LanyardActivity }) => {
-  const [now, setNow] = useState(Date.now());
+	const [now, setNow] = useState(Date.now());
 
-  useEffect(() => {
-    const tick = () => setNow(Date.now());
-    let interval: NodeJS.Timeout;
+	useEffect(() => {
+		const tick = () => setNow(Date.now());
+		let interval: NodeJS.Timeout;
 
-    const delay = 1000 - new Date().getMilliseconds();
-    const timeout = setTimeout(() => {
-      tick();
-      interval = setInterval(tick, 1000);
-    }, delay);
+		const delay = 1000 - new Date().getMilliseconds();
+		const timeout = setTimeout(() => {
+			tick();
+			interval = setInterval(tick, 1000);
+		}, delay);
 
-    return () => {
-      clearTimeout(timeout);
-      if (interval) clearInterval(interval);
-    };
-  }, []);
+		return () => {
+			clearTimeout(timeout);
+			if (interval) clearInterval(interval);
+		};
+	}, []);
 
-  const start = props.activity.timestamps?.start ?? null;
-  const end = props.activity.timestamps?.end ?? null;
-  const createdAt = props.activity.created_at;
+	const start = props.activity.timestamps?.start ?? null;
+	const end = props.activity.timestamps?.end ?? null;
+	const createdAt = props.activity.created_at;
 
-  if (start && end) {
-    const progress =
-      Math.min(Math.max((now - start) / (end - start), 0), 1) * 100;
+	if (start && end) {
+		const progress =
+			Math.min(Math.max((now - start) / (end - start), 0), 1) * 100;
 
-    return (
-      <div class="progress-container">
-        <div class="progress-bar" style={`width:${progress}%`}></div>
-      </div>
-    );
-  } else if (start && !end) {
-    const elapsed = new Date(now - start);
-    const hours = elapsed.getUTCHours();
-    const minutes = elapsed.getUTCMinutes();
-    const seconds = elapsed.getUTCSeconds();
+		return (
+			<div class="progress-container">
+				<div class="progress-bar" style={`width:${progress}%`}></div>
+			</div>
+		);
+	} else if (start && !end) {
+		const elapsed = new Date(now - start);
+		const hours = elapsed.getUTCHours();
+		const minutes = elapsed.getUTCMinutes();
+		const seconds = elapsed.getUTCSeconds();
 
-    return (
-      <span>
-        Elapsed {hours > 0 ? `${hours}:` : ""}
-        {minutes}:{seconds.toString().padStart(2, "0")}
-      </span>
-    );
-  } else if (end && !start) {
-    const left = new Date(end - now);
-    const hours = left.getUTCHours();
-    const minutes = left.getUTCMinutes();
-    const seconds = left.getUTCSeconds();
+		return (
+			<span>
+				Elapsed {hours > 0 ? `${hours}:` : ""}
+				{minutes}:{seconds.toString().padStart(2, "0")}
+			</span>
+		);
+	} else if (end && !start) {
+		const left = new Date(end - now);
+		const hours = left.getUTCHours();
+		const minutes = left.getUTCMinutes();
+		const seconds = left.getUTCSeconds();
 
-    return (
-      <span>
-        {hours > 0 ? `${hours}:` : ""}
-        {minutes}:{seconds.toString().padStart(2, "0")} left
-      </span>
-    );
-  } else if (!start && !end && createdAt) {
-    const elapsed = new Date(now - createdAt);
-    const hours = elapsed.getUTCHours();
-    const minutes = elapsed.getUTCMinutes();
-    const seconds = elapsed.getUTCSeconds();
+		return (
+			<span>
+				{hours > 0 ? `${hours}:` : ""}
+				{minutes}:{seconds.toString().padStart(2, "0")} left
+			</span>
+		);
+	} else if (!start && !end && createdAt) {
+		const elapsed = new Date(now - createdAt);
+		const hours = elapsed.getUTCHours();
+		const minutes = elapsed.getUTCMinutes();
+		const seconds = elapsed.getUTCSeconds();
 
-    return (
-      <span>
-        Elapsed {hours > 0 ? `${hours}:` : ""}
-        {minutes}:{seconds.toString().padStart(2, "0")}
-      </span>
-    );
-  }
+		return (
+			<span>
+				Elapsed {hours > 0 ? `${hours}:` : ""}
+				{minutes}:{seconds.toString().padStart(2, "0")}
+			</span>
+		);
+	}
 
-  return null;
+	return null;
 };
--- src/web/App/Components/Time/index.tsx
+++ src/web/App/Components/Time/index.tsx
@@ -3,27 +3,27 @@ import { useEffect, useState } from "preact/hooks";
 const timeZone = "America/New_York";
 
 export const Time = () => {
-  const [time, setTime] = useState(
-    new Date().toLocaleString(undefined, { timeZone }),
-  );
+	const [time, setTime] = useState(
+		new Date().toLocaleString(undefined, { timeZone }),
+	);
 
-  useEffect(() => {
-    const tick = () => {
-      setTime(new Date().toLocaleString(undefined, { timeZone }));
-    };
-    let interval: NodeJS.Timeout;
+	useEffect(() => {
+		const tick = () => {
+			setTime(new Date().toLocaleString(undefined, { timeZone }));
+		};
+		let interval: NodeJS.Timeout;
 
-    const delay = 1000 - new Date().getMilliseconds();
-    const timeout = setTimeout(() => {
-      tick();
-      interval = setInterval(tick, 1000);
-    }, delay);
+		const delay = 1000 - new Date().getMilliseconds();
+		const timeout = setTimeout(() => {
+			tick();
+			interval = setInterval(tick, 1000);
+		}, delay);
 
-    return () => {
-      clearTimeout(timeout);
-      if (interval) clearInterval(interval);
-    };
-  }, []);
+		return () => {
+			clearTimeout(timeout);
+			if (interval) clearInterval(interval);
+		};
+	}, []);
 
-  return <code>{time}</code>;
+	return <code>{time}</code>;
 };
