69770129af4ffed3dac5004222fd1b86ce6e0637
Seth <git@ipv4.army>
2026-06-14 05:24:05 -0400

    WRGSEH

--- build.ts
+++ build.ts
@@ -37,7 +37,11 @@ const gitmal = await fetch(
 if (gitmal.status !== 200) throw new Error(await gitmal.text());
 
 await Bun.write(path, gitmal);
-try { await Bun.$`chmod +x ${path}`; } catch (_e) {/*ignore*/ }
+try {
+	await Bun.$`chmod +x ${path}`;
+} catch (_e) {
+	/*ignore*/
+}
 const proc = spawn([
 	path,
 	"--minify",
