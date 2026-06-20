f7b6a93b50e595e54bc794dea677cf3046622fab
Seth <git@ipv4.army>
2026-06-20 10:49:57 -0400

    Add NEXT_PUBLIC_URL check and integrate next-umami provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,23 +2,33 @@ import { RootProvider } from "fumadocs-ui/provider/next";
 import "./global.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import UmamiProvider from 'next-umami'
 
 const inter = Inter({
   subsets: ["latin"],
 });
 
+if (!process.env.NEXT_PUBLIC_URL) {
+  throw new Error("NEXT_PUBLIC_URL is not set.");
+}
+if (!process.env.UMAMI_production_ID || !process.env.UMAMI_development_ID) {
+  throw new Error("UMAMI_production_ID or UMAMI_development_ID is not set.");
+}
+
+const isProd = process.env.NODE_ENV === "production";
+
+
 export default function Layout({ children }: LayoutProps<"/">) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
+            <head>
+        <UmamiProvider websiteId={process.env[isProd ? "UMAMI_PRODUCTION_ID" : "UMAMI_DEVELOPMENT_ID"] || ""} src="https://u.ipv4.army/script.js" hostUrl="https://u.ipv4.army" performance={true} />
+      </head>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
     </html>
   );
-}
-
-if (!process.env.NEXT_PUBLIC_URL) {
-  throw new Error("NEXT_PUBLIC_URL is not set.");
 }
 
 export const metadata: Metadata = {
