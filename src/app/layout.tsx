ba5882b21aa2c09568a60eeff820ac4c96455ba8
Seth <git@ipv4.army>
2026-06-20 10:08:23 -0400

    UGH

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,8 +19,6 @@ export default function Layout({ children }: LayoutProps<"/">) {
 
 if (!process.env.NEXT_PUBLIC_URL) {
   throw new Error("NEXT_PUBLIC_URL is not set.");
-} else {
-  console.log(`NEXT_PUBLIC_URL is set to ${process.env.NEXT_PUBLIC_URL}`);
 }
 
 export const metadata: Metadata = {
