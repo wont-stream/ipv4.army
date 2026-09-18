8f96a1f346432df569c1d5a2bbcd94847600894d
Seth <git@ipv4.army>
2026-09-18 00:16:25 -0400

    add 60s internal cache to api

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,69 @@
 import { type Format, makeBadge } from "badge-maker";
 import { serve } from "bun";
+import { BunCache } from "bun-cache";
 import { materialDynamicColors, toCss } from "./util/mdc";
 import index from "./web/index.html";
+
+const cache = {
+	badge: new BunCache(),
+	color: new BunCache(),
+};
 
 const server = serve({
 	routes: {
 		"/": index,
 
 		"/api/color": async (req) => {
+			const { searchParams } = new URL(req.url);
+			const key = searchParams.toString();
+
+			let res = "";
+
 			try {
-				const { searchParams } = new URL(req.url);
+				if (cache.color.hasKey(key)) {
+					res = cache.color.get(key) as string;
+				} else {
+					const src = searchParams.get("src") || undefined;
+					const color = searchParams.get("color") || undefined;
 
-				const src = searchParams.get("src") || undefined;
-				const color = searchParams.get("color") || undefined;
+					const colors = await materialDynamicColors({ src, color });
+					res = toCss(colors);
+					cache.color.put(key, res, 60_000);
+				}
 
-				const colors = await materialDynamicColors({ src, color });
-				const css = toCss(colors);
-
-				return new Response(css,
-					{
-						headers: {
-							"Cache-Control": "public, max-age=31536000, immutable",
-							"content-type": "text/plain",
-							Vary: "Accept-Encoding",
-						},
-					});
+				return new Response(res, {
+					headers: {
+						"Cache-Control": "public, max-age=31536000, immutable",
+						"content-type": "text/plain",
+						Vary: "Accept-Encoding",
+					},
+				});
 			} catch (e) {
 				return new Response(e as string);
 			}
 		},
 
 		"/api/badge": async (req) => {
-			try {
-				const { searchParams } = new URL(req.url);
+			const { searchParams } = new URL(req.url);
+			const key = searchParams.toString();
 
-				return new Response(
-					makeBadge(searchParams.toJSON() as unknown as Format),
-					{
-						headers: {
-							"Cache-Control": "public, max-age=31536000, immutable",
-							"content-type": "image/svg+xml",
-							Vary: "Accept-Encoding",
-						},
+			let res = "";
+
+			try {
+				if (cache.badge.hasKey(key)) {
+					res = cache.badge.get(key) as string;
+				} else {
+					res = makeBadge(searchParams.toJSON() as unknown as Format);
+					cache.color.put(key, res, 60_000);
+				}
+
+				return new Response(res, {
+					headers: {
+						"Cache-Control": "public, max-age=31536000, immutable",
+						"content-type": "image/svg+xml",
+						Vary: "Accept-Encoding",
 					},
-				);
+				});
 			} catch (e) {
 				return new Response(e as string);
 			}
