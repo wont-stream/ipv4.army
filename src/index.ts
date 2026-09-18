994c80948cc2291cfde82ed9ea0cde018cbddf0a
Seth <git@ipv4.army>
2026-09-18 00:02:48 -0400

    add badge api, manually run gc every 60s

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,58 @@
 import { serve } from "bun";
 import { materialDynamicColors, toCss } from "./util/mdc";
 import index from "./web/index.html";
+import { makeBadge, type Format } from 'badge-maker'
+
 
 const server = serve({
 	routes: {
 		"/": index,
 
 		"/api/color": async (req) => {
-			const { searchParams } = new URL(req.url);
+			try {
+				const { searchParams } = new URL(req.url);
 
-			const src = searchParams.get("src") || undefined;
-			const color = searchParams.get("color") || undefined;
+				const src = searchParams.get("src") || undefined;
+				const color = searchParams.get("color") || undefined;
 
-			const colors = await materialDynamicColors({ src, color });
-			const css = toCss(colors);
+				const colors = await materialDynamicColors({ src, color });
+				const css = toCss(colors);
 
-			return new Response(css);
+				return new Response(css);
+			}
+			catch (e) {
+				return new Response(e as string);
+			}
+		},
+
+		"/api/badge": async (req) => {
+
+			try {
+				const { searchParams } = new URL(req.url);
+
+				return new Response(makeBadge(searchParams.toJSON() as unknown as Format),
+					{
+						headers: {
+							"Cache-Control": "public, max-age=31536000, immutable",
+							"content-type": "image/svg+xml",
+							"Vary": "Accept-Encoding"
+						}
+					})
+			}
+			catch (e) {
+				return new Response(e as string);
+			}
 		},
 
 		// for stupidity
 		"/public/*": async (req) => {
-			const url = new URL(req.url);
-			return Response.redirect(url.pathname.replace("/public", ""), 301);
+			try {
+				const url = new URL(req.url);
+				return Response.redirect(url.pathname.replace("/public", ""), 301);
+			}
+			catch (e) {
+				return new Response(e as string);
+			}
 		},
 		"/*": { dir: "./public" },
 	},
@@ -37,3 +68,6 @@ const server = serve({
 });
 
 console.log(`🚀 Server running at ${server.url}`);
+
+// just in case..
+setInterval(Bun.gc, 60_000)
