ae905f274520a72169f238e7898c670e0322947e
Seth <git@ipv4.army>
2026-09-22 13:09:02 -0400

    fix: optimize image component URL parameters for webp output

--- src/web/components/ui/image/index.tsx
+++ src/web/components/ui/image/index.tsx
@@ -1,17 +1,21 @@
 export const Image = (
-	props: React.DetailedHTMLProps<
+	{ alt, src, width, height, ...props }: React.DetailedHTMLProps<
 		React.ImgHTMLAttributes<HTMLImageElement>,
 		HTMLImageElement
 	>,
 ) => {
-	let query = "";
-	if (props.width) query += `&w=${props.width}`;
-	if (props.height) query += `&h=${props.height}`;
+	const params = new URLSearchParams({
+		output: "webp",
+		url: src ?? "",
+		w: width?.toString() ?? "0",
+		h: height?.toString() ?? "0",
+	});
+
 	return (
 		<img
 			{...props}
-			alt={props.alt}
-			src={`https://wsrv.nl?output=webp${query}&url=${encodeURIComponent(props.src as string)}`}
+			alt={alt}
+			src={`https://wsrv.nl?${params}`}
 		/>
 	);
 };
